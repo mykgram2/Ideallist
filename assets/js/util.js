6482fa926ad88a36f235482646cbe8ea0f5049fe
mykgram2 <myklbgram@hotmail.com>
2016-08-08 18:26:44 -0400

    Initial Commit

---
+++ assets/js/util.js
@@ -0,0 +1,38 @@
+// JavaScript Document
+
+
+	/* Sticky Header
+	*/
+	
+	$(window).scroll(function() {
+		
+	if ($(this).scrollTop() > 1){  
+	
+		$('header').addClass("sticky");
+		
+	  }
+	  
+	  else{
+		  
+		$('header').removeClass("sticky");
+		
+	  }
+	  
+	});
+	
+	
+	
+	/* Accordion Function
+	*/
+	
+	/* $('.accordion').on('click', '.accordion-control', function(e){ // When clicked
+	
+	  e.preventDefault();                    // Prevent default action of button
+	  
+	  $(this)                                // Get the element the user clicked on
+	  
+		.next('.accordion-panel')            // Select following panel 
+		.not(':animated')                    // If it is not currently animating
+		.slideToggle();                      // Use slide toggle to show or hide it
+		
+	}); */
