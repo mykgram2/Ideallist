6ee3dcba1c491827057a5bb801cad30f349ec528
mykgram2 <myklbgraham@hotmail.com>
2016-09-09 08:17:08 -0400

    Add files via upload
    General Update

--- assets/js/util.js
+++ assets/js/util.js
@@ -23,11 +23,39 @@
 	});
 	
 	
+    /* Fade In on Scroll
+	*/
+	
+	$(document).ready(function() {
+		
+		/* Every time the window is scrolled ... */
+		$(window).scroll( function(){
+		
+			/* Check the location of each desired element */
+			$('.hideme').each( function(i){
+				
+				var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+				var bottom_of_window = $(window).scrollTop() + $(window).height();
+				
+				/* If the object is completely visible in the window, fade it it */
+				if( bottom_of_window > bottom_of_object ){
+					
+					$(this).animate({'opacity':'1'},500);
+						
+				}
+				
+			}); 
+		
+		});
+		
+	});
+    
+	
 	
 	/* Accordion Function
 	*/
 	
-	/* $('.accordion').on('click', '.accordion-control', function(e){ // When clicked
+	$('.accordion').on('click', '.accordion-control', function(e){ // When clicked
 	
 	  e.preventDefault();                    // Prevent default action of button
 	  
@@ -37,4 +65,4 @@
 		.not(':animated')                    // If it is not currently animating
 		.slideToggle();                      // Use slide toggle to show or hide it
 		
-	}); */
+	});
